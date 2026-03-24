8e29b4ecbd17cf1b3b582e4673e772c882d51565
Derrick Marable <djmarable14@yahoo.com>
2026-03-24 18:29:51 -0500

    Added comment to main function.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// A simple beginning function for now.
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
