f8a0a00e4030928dc09c1750d8dfe906223b19af
Derrick Marable <djmarable14@yahoo.com>
2026-03-25 18:32:27 -0500

    3/25 Added basic file server functionality.

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested %s\n", r.URL.Path)
 	})
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		fmt.Printf("There was an error: %s\n", err.Error())
