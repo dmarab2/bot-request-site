4f1e77daefeee746690d3064abd3cabb227eefe7
Derrick Marable <djmarable14@yahoo.com>
2026-03-28 17:41:01 -0500

    Moved some of the functions/structs into their own files.

--- backend/main.go
+++ backend/main.go
@@ -7,66 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/dmarab2/bot-request-site/backend/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-
-type jsonRequest struct {
-	ID          int       `json:"id"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	RequestText string    `json:"request_text"`
-}
-
-type apiConfig struct {
-	db     *database.Queries
-	secret string
-}
-
-// helper function to write a JSON error if something goes wrong during handling
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	type errorStruct struct {
-		ErrorString string `json:"error"`
-	}
-	jsonErr := errorStruct{
-		ErrorString: msg,
-	}
-	data, err := json.Marshal(jsonErr)
-	if err != nil {
-		log.Printf("Error marshaling json: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(data)
-}
-
-// basic helper function to write a JSON bytearray to the address of the handler that called it
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	data, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshaling json: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(data)
-}
-
-func turnRequestToJSON(databaseRequest database.Request) jsonRequest {
-	jsonRequest := jsonRequest{
-		ID:          int(databaseRequest.ID),
-		CreatedAt:   databaseRequest.CreatedAt,
-		UpdatedAt:   databaseRequest.UpdatedAt,
-		RequestText: databaseRequest.RequestText,
-	}
-	return jsonRequest
-}
 
 func (cfg *apiConfig) createRequestWriter(w http.ResponseWriter, req *http.Request) {
 	type parameters struct {
---
+++ backend/structs.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"time"
+
+	"github.com/dmarab2/bot-request-site/backend/internal/database"
+)
+
+type jsonRequest struct {
+	ID          int       `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	RequestText string    `json:"request_text"`
+}
+
+type apiConfig struct {
+	db     *database.Queries
+	secret string
+}
