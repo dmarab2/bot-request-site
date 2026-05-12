e6d6e14d3b2f946779a03e96e03cbf79269b74a3
Derrick Marable <djmarable14@yahoo.com>
2026-05-11 23:05:40 -0500

    Added comment in helper_functions.go

--- backend/helper_functions.go
+++ backend/helper_functions.go
@@ -1,5 +1,6 @@
 // helper_functions mostly contains functions that are often used in the main package to write DRY code but aren't core server functionality.
 // this includes JSON conversion, middleware, and response helpers.
+// some functions here are pure, mostly for making new structs.
 package main
 
 import (
