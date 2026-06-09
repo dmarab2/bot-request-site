b904434c41f7b0d5e17a9afaf3cf94626c668c75
Derrick Marable <djmarable14@yahoo.com>
2026-06-08 22:14:26 -0500

    Added test for respondWithError.

--- backend/helper_functions_test.go
+++ backend/helper_functions_test.go
@@ -1,9 +1,34 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 )
 
+func TestRespondWithError(t *testing.T) {
+	type jsonDecoder struct {
+		TestErrorHolder string `json:"error"`
+	}
+	w := httptest.NewRecorder()
+	respondWithError(w, 500, "TEST ERROR")
+	resp := w.Result()
+	defer resp.Body.Close()
+	if resp.StatusCode != 500 {
+		t.Errorf("Status code returned was %d instead of %d", resp.StatusCode, 500)
+	}
+	testStruct := jsonDecoder{}
+	if err := json.NewDecoder(resp.Body).Decode(&testStruct); err != nil {
+		t.Errorf("Error decoding the test error message: %s", err.Error())
+	}
+	if testStruct.TestErrorHolder != "TEST ERROR" {
+		t.Errorf("testErrorHolder is %s instead of TEST ERROR", testStruct.TestErrorHolder)
+	}
+
+}
+
+// TestValidateClaimPassword tests every possible output from validateClaimPassword to make sure that empty strings,
+// nonexistent submissions, and valid submissions receive the correct response.
 func TestValidateClaimPassword(t *testing.T) {
 	testPassword := "testPassword"
 	testClaim := requestClaimInsert{requestID: 40, password: &testPassword}
@@ -24,6 +49,7 @@ func TestValidateClaimPassword(t *testing.T) {
 	}
 }
 
+// TestNormalizeTagName makes sure that all strings are normalized to the same format no matter what the string contains.
 func TestNormalizeTagName(t *testing.T) {
 	testString := "Ran Yakumo"
 	returnedString := normalizeTagName(testString)
